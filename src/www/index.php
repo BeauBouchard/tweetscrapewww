017ca3d80e6e6cd4723d84c9bff51d6e638e161c
Beau Bouchard <beau@beaubouchard.com>
2014-02-11 15:02:25 -0500

    Create index.php

---
+++ src/www/index.php
@@ -0,0 +1,23 @@
+<?php
+/*		
+ *		Filename: index.php
+ *		Description: Top part of the page, controls which JS to add, which theme/css to use
+ *			tweetscrapewww is a web portion of a larger project tweetscrape. This will display gathered tweets
+ * 		Author: Beau Bouchard (@BeauBouchard)
+ *		Part of Repo: https://github.com/BeauBouchard/
+ *			License: GNU General Public License 
+ *			License URI: https://www.gnu.org/licenses/gpl.html
+ *			tweetscrapewww is distributed under the terms of the GNU GPL © 2013-2014 Beau Bouchard
+ *		Version: 1.0.0.1
+ *		
+ */
+
+//include Objects here
+
+//include datalayer if functionality requires it
+
+
+// once page is generated, no longer able to preprocess data.
+
+
+?>
